d1e9a6fe073077da7352a1e57ab458d2a080c98a
bafflepilled <ed@edwardflex.info>
2024-03-04 00:43:07 -0700

    jdk install for intel macs

--- scripts/install_jdk.sh
+++ scripts/install_jdk.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-/usr/bin/curl -L "https://ewr1.vultrobjects.com/miladycraft/OpenJDK17U-jdk_aarch64_mac_hotspot_17.0.3_7.pkg" -o temurin17.pkg
+if [[ $(uname -m) == "arm64" ]]; then
+    /usr/bin/curl -L "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.10%2B7/OpenJDK17U-jdk_aarch64_mac_hotspot_17.0.10_7.pkg" -o temurin17.pkg
+else
+    /usr/bin/curl -L "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.10%2B7/OpenJDK17U-jdk_x64_mac_hotspot_17.0.10_7.pkg" -o temurin17.pkg
+fi
+
 /usr/sbin/installer -pkg ./temurin17.pkg -target /
 /bin/rm temurin17.pkg
 
