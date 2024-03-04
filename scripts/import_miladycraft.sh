0fa150b7a40d8b148587039c743a7fd43da18de6
bafflepilled <ed@edwardflex.info>
2024-03-04 00:21:55 -0700

    install Miladycraft through PollyMC

---
+++ scripts/import_miladycraft.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/usr/bin/su $USER -c "/Applications/PollyMC.app/Contents/MacOS/pollymc -I ./Miladycraft\ 1.4.zip"
+exit 0
--- scripts/install_miladycraft.sh
+++ scripts/install_miladycraft.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 /usr/bin/su $USER -c  "/usr/sbin/installer -pkg ./Miladycraft.pkg -target CurrentUserHomeDirectory"
-
 exit 0
