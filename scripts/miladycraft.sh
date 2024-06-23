88ab25214a0d30abdc06e57814e1b83afe80ea3b
bafflepilled <ed@edwardflex.info>
2024-06-23 04:38:51 -0500

    1.0

--- scripts/miladycraft.sh
+++ scripts/miladycraft.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+cd ~/Library/Application\ Support/PollyMC/instances
 
-/Applications/PollyMC.app/Contents/MacOS/pollymc -I ~/Library/Application\ Support/PollyMC/MiladyCraft_localhost.zip
-program_pid=$!
+if [ -d "Miladycraft 1.5" ]; then
+    rm -rf Miladycraft\ 1.5
+fi
 
-wait $program_pid
+mkdir -p Miladycraft\ 1.5/.minecraft
+/usr/bin/unzip -q MiladyCraft\ 1.5.zip -d MiladyCraft\ 1.5
+rm MiladyCraft\ 1.5.zip
 
-rm ~/Library/Application\ Support/PollyMC/MiladyCraft_localhost.zip
+if [ -d "Miladycraft 1.4" ]; then
+    cp Miladycraft\ 1.4/.minecraft/.sl_password MiladyCraft\ 1.5/.minecraft/.sl_password
+    cp -rf Miladycraft\ 1.4/.minecraft/XaeroWaypoints MiladyCraft\ 1.5/.minecraft/XaeroWaypoints
+    cp -rf Miladycraft\ 1.4/.minecraft/XaeroWorldMap MiladyCraft\ 1.5/.minecraft/XaeroWorldMap
+fi
 
 exit 0
